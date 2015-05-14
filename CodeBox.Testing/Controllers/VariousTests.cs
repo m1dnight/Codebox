961d45f45c3b90392342b964832b58d203579abb
Christophe De Troyer <christophe.detroyer@gmail.com>
2015-05-14 15:14:05 +0200

    Created extra class to unit test protected methods
    Created a derived class for the CustomMembershipPRovider such that we can
    test the inner methods:

---
+++ CodeBox.Testing/Controllers/VariousTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeBox.Testing.Controllers
+{
+    [TestClass]
+    public class VariousTests
+    {
+        [TestMethod]
+        public void TestEmailValidation()
+        {
+        }
+        [TestMethod]
+        public void TestPasswordValidation()
+        {
+        }
+    }
+}
---
+++ CodeBox.Testing/DerivedMembershipController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+using CodeBox.WebUI.Infrastructure.Concrete;
+
+namespace CodeBox.Testing
+{
+    class DerivedMembershipController : CustomMembershipProvider
+    {
+        public void ValidatePassword(ValidatePasswordEventArgs args)
+        {
+            base.OnValidatingPassword(args);
+        }
+    }
+}
