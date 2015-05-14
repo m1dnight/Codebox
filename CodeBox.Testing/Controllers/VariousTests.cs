cc6617490626dd6b69a8cee53d7f3494336c2c55
Christophe De Troyer <christophe.detroyer@gmail.com>
2015-05-14 15:37:40 +0200

    created unit test for password strength

--- CodeBox.Testing/Controllers/VariousTests.cs
+++ CodeBox.Testing/Controllers/VariousTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Web.Security;
+using CodeBox.WebUI.Infrastructure.Concrete;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CodeBox.Testing.Controllers
@@ -13,6 +15,23 @@ namespace CodeBox.Testing.Controllers
         [TestMethod]
         public void TestPasswordValidation()
         {
+            DerivedMembershipController mp = new DerivedMembershipController();
+            String[] valid = new[] {"abc123%"};
+            String[] invalid = new[] {"abc123", "short", "nospecialchars", "nonumbers"};
+            foreach (String s in valid)
+            {
+                ValidatePasswordEventArgs args = new ValidatePasswordEventArgs("admin", s, true);
+                mp.ValidatePassword(args);
+                Assert.AreEqual(false, args.Cancel);
+            }
+            foreach (String s in invalid)
+            {
+                ValidatePasswordEventArgs args = new ValidatePasswordEventArgs("admin", s, true);
+                mp.ValidatePassword(args);
+                Assert.AreEqual(true, args.Cancel);
+            }
+
+
         }
     }
 }
--- CodeBox.Testing/DerivedMembershipController.cs
+++ CodeBox.Testing/DerivedMembershipController.cs
@@ -11,6 +11,7 @@ namespace CodeBox.Testing
     {
         public void ValidatePassword(ValidatePasswordEventArgs args)
         {
+                
             base.OnValidatingPassword(args);
         }
     }
